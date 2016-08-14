d71669c212efbef413ff6ae1ad090e0feffcd16c
franci22 <2002nasce@gmail.com>
2016-08-14 18:59:46 +0200

    Added something.

--- core.php
+++ core.php
@@ -9,7 +9,7 @@ $update = json_decode($content, true);
 $msg = $update["message"]["caption"];
 $chatID = $update["chat"]["id"];
 
-if($msg == "#hashtag"){ //Set #hashtag.
+if($msg == "#hashtag" && $chatID < 0/*If the ChatID is lower than 0 it's a group.*/){ //TODO: Set #hashtag.
 
   $file_id_array = $update["message"]["photo"];
   foreach ($file_id_array as $value) {
@@ -31,4 +31,8 @@ if($msg == "#hashtag"){ //Set #hashtag.
   $name = $name[1]; //Filename.
   file_put_contents($name, fopen($file_url, "r")); //Download.
 
+  //sm($chatID, "*Media saved!*"); //Uncomment this line if you want the bot notifies the rescuing.
+
+} else if ($chatID > 0) { //If someone send a private message to the Bot.
+  sm($chatID, "Hi! I work only in the groups!\nI'm an Open Source bot, you can see my code on [GitHub](https://github.com/franci22/DownImgBot)!");
 }
